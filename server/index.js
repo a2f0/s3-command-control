2316696f5187bb478abf654216259100c57852ba
Dan Sullivan <dan@thinkspan.com>
2022-10-04 08:22:45 -0400

    fix: stub out server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const express = require('express')
+const app = express()
+const port = 1338
+
+app.post('/', (req, res) => {
+  res.statusCode = 201;
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
