effa63fe792bf83555d58a93f2a8449a1a35e38e
Dan Sullivan <dan@thinkspan.com>
2022-10-05 09:38:02 -0400

    feat: middleware

--- server/index.js
+++ server/index.js
@@ -3,10 +3,23 @@ const express = require('express')
 const app = express()
 const port = 1338
 
-app.post('/', (req, res) => {
+function postHandler(req, res, next) {
   res.statusCode = 201;
   res.send()
-})
+  next()
+}
+
+const beforeMiddleware = function(req, res, next) {
+  console.log('Before middleware triggered');
+  next();
+}
+
+const afterMiddleware = function(req, res, next) {
+  console.log('After middleware triggered');
+  next();
+}
+
+app.post('/', beforeMiddleware, postHandler, afterMiddleware)
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
